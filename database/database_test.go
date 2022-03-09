9afc4a0129d6b4a0ad6914a6c7d0f746611e5529
PJ <peterjan.brone@gmail.com>
2022-03-09 17:54:10 +0100

    Add docstrings

--- database/database_test.go
+++ database/database_test.go
@@ -1,7 +1,12 @@
 package database
 
 const (
-	MongoTestUsername   = "admin"
-	MongoTestPassword   = "aO4tV5tC1oU3oQ7u"
+	// MongoTestUsername is the username for the test database
+	MongoTestUsername = "admin"
+
+	// MongoTestPassword is the password for the test database
+	MongoTestPassword = "aO4tV5tC1oU3oQ7u"
+
+	// MongoTestConnString is the connection string for the test database
 	MongoTestConnString = "mongodb://localhost:37017"
 )
